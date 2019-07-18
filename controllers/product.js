55452f0c8f30ea3fdb2a16bbb35674afbf358de3
fahmi023 <fahmiaditya860@gmail.com>
2019-07-18 11:28:55 +0700

    update

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,18 @@
 const Product = require('../models/product');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports.getIndexProduct = (req, res) => {
+    jwt.verify(req.token, process.env.SECRETKEY, (error, authData) => {
+        if (error) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'OK',
+                authData: authData
+            })
+        }
+    })
 }
