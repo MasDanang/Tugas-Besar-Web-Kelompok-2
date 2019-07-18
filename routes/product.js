fda584c0f73a18b7cd292d029c554ddd3b9ba79b
fahmi023 <fahmiaditya860@gmail.com>
2019-07-18 12:11:15 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const productController = require('../controllers/product');
 
+const auth = require('../configs/auth');
+
 router.get('/', productController.getIndexProduct);
 
 module.exports = router;
