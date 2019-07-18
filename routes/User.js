16f5c86674036fce7ddff68181a7c925af11233f
danang10496 <danang10496@gmail.com>
2019-07-18 12:32:25 +0700

    lanjut

--- routes/User.js
+++ routes/User.js
@@ -5,8 +5,5 @@ const UserController = require('../controllers/User');
 const router = express.Router();
 
 router.post('/register', UserController.postRegister);
-router.post('/login', UserController.postLogin);
-router.post('/delete', UserController.postDestroy);
-router.post('/update', UserController.postUpdate);
 
 module.exports = router;    
